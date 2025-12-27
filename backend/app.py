0f1feb4f06ff06c7aaea911bf26227c9aa7e1a00
chaitanyaa <159754889+chaitanya7113@users.noreply.github.com>
2025-12-27 21:40:48 +0530

    upadat in backend

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,12 @@ import tensorflow as tf
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import io
+import os
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 # =========================
 # APP SETUP
